fae28790792e8a9b3c9a05f44983fe965121e269
Ace <georgesroberto21@gmail.com>
2023-06-22 23:57:49 +0300

    Goodnight

--- birthday/functionality.js
+++ birthday/functionality.js
@@ -1,7 +1,7 @@
-const daysEl
-const hoursEl
-const minutesEl
-const secondsEl
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
 
 
 const newYears = '5th July 2023';
